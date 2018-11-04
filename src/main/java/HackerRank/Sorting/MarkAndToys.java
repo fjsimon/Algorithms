0b729813d5ecf8586096f6a181706edff5e48507
frs08 <francisco.simon@sky.uk>
2018-11-04 10:51:14 +0000

    Added ArrayManipulation and MarkAndToys solutions

---
+++ src/main/java/HackerRank/Sorting/MarkAndToys.java
@@ -0,0 +1,55 @@
+package HackerRank.Sorting;
+
+import java.io.*;
+import java.util.*;
+
+public class MarkAndToys {
+
+    // Complete the maximumToys function below.
+    static int maximumToys(int[] prices, int k) {
+
+        int items = 0;
+        int sum = 0;
+
+        Arrays.sort(prices);
+        for(int i=0; i<prices.length; i++) {
+            if(sum <= k) {
+                sum += prices[i];
+                items++;
+            }
+        }
+
+        return items-1;
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String[] nk = scanner.nextLine().split(" ");
+
+        int n = Integer.parseInt(nk[0]);
+
+        int k = Integer.parseInt(nk[1]);
+
+        int[] prices = new int[n];
+
+        String[] pricesItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < n; i++) {
+            int pricesItem = Integer.parseInt(pricesItems[i]);
+            prices[i] = pricesItem;
+        }
+
+        int result = maximumToys(prices, k);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
