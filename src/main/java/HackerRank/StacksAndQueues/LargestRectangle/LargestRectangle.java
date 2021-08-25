c15dee6e6de0563c8564e67e5402cb66c6fb84f8
fjsimon <fjlopez.mail@gmail.com>
2021-08-25 21:09:58 +0100

    Added LargestRectangle

---
+++ src/main/java/HackerRank/StacksAndQueues/LargestRectangle/LargestRectangle.java
@@ -0,0 +1,8 @@
+package HackerRank.StacksAndQueues.LargestRectangle;
+
+/**
+ * https://www.hackerrank.com/challenges/largest-rectangle/problem?h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=stacks-queues
+ */
+public class LargestRectangle {
+
+}
