7107c67416056b8cc5b56d9a9a0d931c7031f053
fjsimon <fjlopez.mail@gmail.com>
2021-08-27 20:27:02 +0100

    Added MixMaxRiddle

---
+++ src/main/java/HackerRank/StacksAndQueues/MixMaxRiddle/MixMaxRiddle.java
@@ -0,0 +1,55 @@
+package HackerRank.StacksAndQueues.MixMaxRiddle;
+
+import java.io.*;
+import java.util.*;
+
+/**
+ * https://www.hackerrank.com/challenges/min-max-riddle/problem?h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=stacks-queues
+ *
+ */
+
+public class MixMaxRiddle {
+
+    static long[] riddle(long[] arr) {
+
+        long[] result = new long[arr.length];
+
+
+        return result;
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        long[] arr = new long[n];
+
+        String[] arrItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < n; i++) {
+            long arrItem = Long.parseLong(arrItems[i]);
+            arr[i] = arrItem;
+        }
+
+        long[] res = riddle(arr);
+
+        for (int i = 0; i < res.length; i++) {
+            bufferedWriter.write(String.valueOf(res[i]));
+
+            if (i != res.length - 1) {
+                bufferedWriter.write(" ");
+            }
+        }
+
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
