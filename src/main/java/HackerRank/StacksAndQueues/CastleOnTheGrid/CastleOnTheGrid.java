2f82b7b86c5f73882470c6bb2aaf3572acb49e11
fjsimon <fjlopez.mail@gmail.com>
2021-08-30 15:15:42 +0100

    Added CastleOnTheGrid

---
+++ src/main/java/HackerRank/StacksAndQueues/CastleOnTheGrid/CastleOnTheGrid.java
@@ -0,0 +1,67 @@
+package HackerRank.StacksAndQueues.CastleOnTheGrid;
+
+import java.io.*;
+import java.util.List;
+import java.util.stream.IntStream;
+
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'minimumMoves' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts following parameters:
+     *  1. STRING_ARRAY grid
+     *  2. INTEGER startX
+     *  3. INTEGER startY
+     *  4. INTEGER goalX
+     *  5. INTEGER goalY
+     */
+
+    public static int minimumMoves(List<String> grid, int startX, int startY, int goalX, int goalY) {
+        // Write your code here
+
+        return 0;
+    }
+
+}
+
+public class CastleOnTheGrid {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<String> grid = IntStream.range(0, n).mapToObj(i -> {
+            try {
+                return bufferedReader.readLine();
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        })
+                .collect(toList());
+
+
+        String[] firstMultipleInput = bufferedReader.readLine().
+                replaceAll("\\s+$", "").split(" ");
+
+        int startX = Integer.parseInt(firstMultipleInput[0]);
+
+        int startY = Integer.parseInt(firstMultipleInput[1]);
+
+        int goalX = Integer.parseInt(firstMultipleInput[2]);
+
+        int goalY = Integer.parseInt(firstMultipleInput[3]);
+
+        int result = Result.minimumMoves(grid, startX, startY, goalX, goalY);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
