4a08cad93906c75061bac7446766660d3e4f9029
fjsimon <fjlopez.mail@gmail.com>
2021-08-31 20:25:50 +0100

    Added CastleOnTheGrid

--- src/main/java/HackerRank/StacksAndQueues/CastleOnTheGrid/CastleOnTheGrid.java
+++ src/main/java/HackerRank/StacksAndQueues/CastleOnTheGrid/CastleOnTheGrid.java
@@ -1,31 +1,76 @@
 package HackerRank.StacksAndQueues.CastleOnTheGrid;
 
 import java.io.*;
+import java.util.Arrays;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 import java.util.stream.IntStream;
 
 import static java.util.stream.Collectors.toList;
 
+/**
+ *
+ * https://www.hackerrank.com/challenges/castle-on-the-grid/problem?h_l=interview&isFullScreen=false&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=stacks-queues
+ *
+ */
 class Result {
 
-    /*
-     * Complete the 'minimumMoves' function below.
-     *
-     * The function is expected to return an INTEGER.
-     * The function accepts following parameters:
-     *  1. STRING_ARRAY grid
-     *  2. INTEGER startX
-     *  3. INTEGER startY
-     *  4. INTEGER goalX
-     *  5. INTEGER goalY
-     */
+    static final int[] X_OFFSETS = { -1, 0, 1, 0 };
+    static final int[] Y_OFFSETS = { 0, 1, 0, -1 };
+
 
     public static int minimumMoves(List<String> grid, int startX, int startY, int goalX, int goalY) {
-        // Write your code here
+        if (startX == goalX && startY == goalY) {
+            return 0;
+        }
 
-        return 0;
+        int size = grid.size();
+
+        int[][] moves = new int[size][size];
+        IntStream.range(0, size).forEach(x -> Arrays.fill(moves[x], -1));
+
+        moves[startX][startY] = 0;
+        Queue<Point> queue = new LinkedList<>();
+        queue.offer(new Point(startX, startY));
+
+        while (true) {
+            Point head = queue.poll();
+            for (int i = 0; i < X_OFFSETS.length; i++) {
+                int nextX = head.x;
+                int nextY = head.y;
+
+                while (isOpen(grid, nextX + X_OFFSETS[i], nextY + Y_OFFSETS[i])) {
+                    nextX += X_OFFSETS[i];
+                    nextY += Y_OFFSETS[i];
+
+                    if (nextX == goalX && nextY == goalY) {
+                        return moves[head.x][head.y] + 1;
+                    }
+
+                    if (moves[nextX][nextY] < 0) {
+                        moves[nextX][nextY] = moves[head.x][head.y] + 1;
+                        queue.offer(new Point(nextX, nextY));
+                    }
+                }
+            }
+        }
     }
 
+    static boolean isOpen(List<String> grid, int x, int y) {
+
+        return x >= 0 && x < grid.size() && y >= 0 && y < grid.size() && grid.get(x).charAt(y) == '.';
+    }
+}
+
+class Point {
+    int x;
+    int y;
+
+    Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
 }
 
 public class CastleOnTheGrid {
@@ -41,8 +86,7 @@ public class CastleOnTheGrid {
             } catch (IOException ex) {
                 throw new RuntimeException(ex);
             }
-        })
-                .collect(toList());
+        }).collect(toList());
 
 
         String[] firstMultipleInput = bufferedReader.readLine().
