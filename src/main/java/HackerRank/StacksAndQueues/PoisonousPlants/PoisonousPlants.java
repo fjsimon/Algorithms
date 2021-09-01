908e23c8781076c1a492b99b831d1c8bf57de635
fjsimon <fjlopez.mail@gmail.com>
2021-09-01 19:20:15 +0100

    Added PoisonousPlants

---
+++ src/main/java/HackerRank/StacksAndQueues/PoisonousPlants/PoisonousPlants.java
@@ -0,0 +1,9 @@
+package HackerRank.StacksAndQueues.PoisonousPlants;
+
+/**
+ * https://www.hackerrank.com/challenges/poisonous-plants/problem?
+ *  h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=stacks-queues
+ *
+ */
+public class PoisonousPlants {
+}
