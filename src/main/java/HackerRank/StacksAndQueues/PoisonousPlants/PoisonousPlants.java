9ec6346504a0a0005de157837650bcdc5b2c8058
fjsimon <fjlopez.mail@gmail.com>
2021-09-02 18:28:06 +0100

    Added PoisonousPlants

--- src/main/java/HackerRank/StacksAndQueues/PoisonousPlants/PoisonousPlants.java
+++ src/main/java/HackerRank/StacksAndQueues/PoisonousPlants/PoisonousPlants.java
@@ -1,9 +1,51 @@
 package HackerRank.StacksAndQueues.PoisonousPlants;
 
+import java.io.*;
+import java.util.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.toList;
+
 /**
  * https://www.hackerrank.com/challenges/poisonous-plants/problem?
  *  h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=stacks-queues
  *
  */
+class Result {
+
+    /*
+     * Complete the 'poisonousPlants' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts INTEGER_ARRAY p as parameter.
+     */
+
+    public static int poisonousPlants(List<Integer> p) {
+        // Write your code here
+
+        return 0;
+    }
+
+}
+
 public class PoisonousPlants {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<Integer> p = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                .map(Integer::parseInt)
+                .collect(toList());
+
+        int result = Result.poisonousPlants(p);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
 }
