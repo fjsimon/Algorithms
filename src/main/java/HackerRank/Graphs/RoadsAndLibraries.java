8c455aec1186bd251f4c4d7dd209c13fd888b1e6
fjsimon <fjlopez.mail@gmail.com>
2021-09-06 19:43:29 +0100

    Added RoadsAndLibraries

---
+++ src/main/java/HackerRank/Graphs/RoadsAndLibraries.java
@@ -0,0 +1,77 @@
+package HackerRank.Graphs;
+
+import java.io.*;
+import java.util.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'roadsAndLibraries' function below.
+     *
+     * The function is expected to return a LONG_INTEGER.
+     * The function accepts following parameters:
+     *  1. INTEGER n
+     *  2. INTEGER c_lib
+     *  3. INTEGER c_road
+     *  4. 2D_INTEGER_ARRAY cities
+     */
+
+    public static long roadsAndLibraries(int n, int c_lib, int c_road, List<List<Integer>> cities) {
+        // Write your code here
+
+        return 0;
+    }
+
+}
+
+public class RoadsAndLibraries {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int q = Integer.parseInt(bufferedReader.readLine().trim());
+
+        IntStream.range(0, q).forEach(qItr -> {
+            try {
+                String[] firstMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+                int n = Integer.parseInt(firstMultipleInput[0]);
+
+                int m = Integer.parseInt(firstMultipleInput[1]);
+
+                int c_lib = Integer.parseInt(firstMultipleInput[2]);
+
+                int c_road = Integer.parseInt(firstMultipleInput[3]);
+
+                List<List<Integer>> cities = new ArrayList<>();
+
+                IntStream.range(0, m).forEach(i -> {
+                    try {
+                        cities.add(
+                                Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                                        .map(Integer::parseInt)
+                                        .collect(toList())
+                        );
+                    } catch (IOException ex) {
+                        throw new RuntimeException(ex);
+                    }
+                });
+
+                long result = Result.roadsAndLibraries(n, c_lib, c_road, cities);
+
+                bufferedWriter.write(String.valueOf(result));
+                bufferedWriter.newLine();
+
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
