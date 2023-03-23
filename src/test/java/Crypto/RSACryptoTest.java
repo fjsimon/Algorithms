a1b4cd7b64e5ce5b999b79ac152786002f6625ef
fjsimon <fjlopez.mail@gmail.com>
2023-03-23 15:56:45 +0000

    Updated RSACryptoTest.java and .gitignore

--- src/test/java/Crypto/RSACryptoTest.java
+++ src/test/java/Crypto/RSACryptoTest.java
@@ -1,13 +1,21 @@
 package Crypto;
 
+import lombok.SneakyThrows;
 import org.junit.jupiter.api.Test;
+
+import javax.crypto.Cipher;
+import java.io.FileOutputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Base64;
+
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 
 class RSACryptoTest {
 
     @Test
-    public void rsa_message_test() throws Exception {
+    public void rsa_text_message_test() {
 
         RSACrypto.generateKeys();
 
@@ -15,6 +23,37 @@ class RSACryptoTest {
         String encryptedMsg = RSACrypto.encrypt(secretMessage);
         String decryptedMsg = RSACrypto.decrypt(encryptedMsg);
         assertThat(secretMessage, is(decryptedMsg));
+    }
+
+    @Test
+    @SneakyThrows
+    public void rsa_text_files_test() {
+
+        Path tempFile = Files.createTempFile("temp", "txt");
+        Files.writeString(tempFile, "Keep It For You");
+        byte[] fileBytes = Files.readAllBytes(tempFile);
+
+        Cipher encryptCipher = Cipher.getInstance("RSA");
+        encryptCipher.init(Cipher.ENCRYPT_MODE, RSACrypto.getPublicKey());
+
+        byte[] encryptedFileBytes = encryptCipher.doFinal(fileBytes);
+
+        try (FileOutputStream stream = new FileOutputStream(tempFile.toFile())) {
+            stream.write(encryptedFileBytes);
+        }
+
+        encryptedFileBytes = Files.readAllBytes(tempFile);
+
+        Cipher decryptCipher = Cipher.getInstance("RSA");
+        decryptCipher.init(Cipher.DECRYPT_MODE, RSACrypto.getPrivateKey());
+        byte[] decryptedFileBytes = decryptCipher.doFinal(encryptedFileBytes);
+
+        try (FileOutputStream stream = new FileOutputStream(tempFile.toFile())) {
+            stream.write(decryptedFileBytes);
+        }
+
+        String fileContent = Files.readString(tempFile);
+        assertThat("Keep It For You", is(fileContent));
     }
 
 }
