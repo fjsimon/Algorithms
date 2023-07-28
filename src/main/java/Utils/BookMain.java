4e9a8664440e418e75a13cb715e3827615c61508
fjsimon <fjlopez.mail@gmail.com>
2023-07-28 12:17:59 +0100

    Added book example using jaxb.

---
+++ src/main/java/Utils/BookMain.java
@@ -0,0 +1,72 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Utils;
+
+import Utils.model.Book;
+import Utils.model.Bookstore;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Marshaller;
+import javax.xml.bind.Unmarshaller;
+
+public class BookMain {
+
+    private static final String BOOKSTORE_XML = "bookstore-jaxb.xml";
+
+    public static void main(String[] args) throws JAXBException, IOException {
+
+        var bookList = new ArrayList<Book>();
+
+        // create books
+        var book1 = new Book();
+        book1.setIsbn("978-0060554736");
+        book1.setName("The Game");
+        book1.setAuthor("Neil Strauss");
+        book1.setPublisher("Harpercollins");
+        bookList.add(book1);
+
+        var book2 = new Book();
+        book2.setIsbn("978-3832180577");
+        book2.setName("Feuchtgebiete");
+        book2.setAuthor("Charlotte Roche");
+        book2.setPublisher("Dumont Buchverlag");
+        bookList.add(book2);
+
+        // create bookstore, assigning book
+        var bookstore = new Bookstore();
+        bookstore.setName("Fraport Bookstore");
+        bookstore.setLocation("Frankfurt Airport");
+        bookstore.setBookList(bookList);
+
+        // create JAXB context and instantiate marshaller
+        JAXBContext context = JAXBContext.newInstance(Bookstore.class);
+        Marshaller m = context.createMarshaller();
+        m.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
+
+        // Write to System.out
+        m.marshal(bookstore, System.out);
+
+        // Write to File
+        m.marshal(bookstore, new File(BOOKSTORE_XML));
+
+        // get variables from our xml file, created before
+        System.out.println();
+        System.out.println("Output from our XML File: ");
+        Unmarshaller um = context.createUnmarshaller();
+        Bookstore bookstore2 = (Bookstore) um.unmarshal(new FileReader(
+                BOOKSTORE_XML));
+        List<Book> list = bookstore2.getBooksList();
+        for (Book book : list) {
+            System.out.println("Book: " + book.getName() + " from "
+                    + book.getAuthor());
+        }
+    }
+}
