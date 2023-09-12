69e27ee05a0483937aa1565193627d1ac924d6ed
fjsimon <fjlopez.mail@gmail.com>
2023-09-12 08:48:25 +0100

    Remove comment App.java

--- src/main/java/Kata/App.java
+++ src/main/java/Kata/App.java
@@ -3,10 +3,7 @@ package Kata;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-/**
- * Hello world!
- *
- */
+
 public class App 
 {
     private static final Logger logger = LoggerFactory.getLogger(App.class);
